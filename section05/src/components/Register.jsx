9e56b94069e156ba6ca26ccc7e18eae6db3ddd09
noheejoong <noheejoong@gmail.com>
2024-12-28 11:03:50 +0900

    5.10

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState, useRef } from "react";
 //간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
@@ -6,42 +6,62 @@ import {useState} from 'react'
 // 4. 자기소개
 
 const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+  const countRef = useRef(0);
+  const inputRef = useRef();
 
-    const [input, setInput] = useState({
-        name:"",
-        birth:"",
-        country:"",
-        bio:""
-    });
+  const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
-    const onChange = (e) => {
-        console.log(e.target.name, e.target.value);
-        setInput({...input,
-        [e.target.name]:e.target.value
-        })
+  const onSubmit = (e) => {
+    if (input.name === "") {
+      inputRef.current.focus();
     }
+  };
 
-    return (
-        <div>
-            <div>
-            <input name="name" value={input.name} onChange={onChange} placeholder={"이름"}/>
-            </div>
-            <div>
-            <input name="birth" value={input.birth} onChange={onChange} type="date" />
-            </div>
-            <div>
-            <select name="country" value={input.country} onChange={onChange}>
-                <option value=''></option>
-                <option value='kr'>한국</option>
-                <option value='us'>미국</option>
-                <option value='uk'>영국</option>
-                </select>
-            </div>
-            <div>
-                <textarea name="bio" value={input.bio} onChange={onChange}/>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
+      </div>
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+      <div>
+        <button onClick={onSubmit}>제출</button>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
